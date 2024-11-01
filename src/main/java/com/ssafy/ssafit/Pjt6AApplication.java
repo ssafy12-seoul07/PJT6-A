721a91fbd98f01aad671be8ea07c9a414e11c9ac
newbiejanghan <bjh4135@gmail.com>
2024-11-01 11:30:24 +0900

    setup

---
+++ src/main/java/com/ssafy/ssafit/Pjt6AApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pjt6AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pjt6AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ssafit/Pjt6AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pjt6AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
